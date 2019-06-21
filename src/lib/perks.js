3068f07db09d7f753383c9874275ea839e90f7f2
Jaid <jaid.jsx@gmail.com>
2019-06-21 07:04:12 +0200

    Finished perk meta data
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/lib/perks.js
+++ src/lib/perks.js
@@ -3,7 +3,9 @@ import {titleCase} from "change-case"
 
 export default perks.map(perk => ({
   title: perk.id |> titleCase,
-  teachable: true,
+  ingameId: perk.id,
+  owner: false,
+  level: 30,
   rarity: "rare",
   visible: true,
   ...perk,
