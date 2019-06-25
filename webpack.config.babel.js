35dac94f69858c3193a53d897d86d9e387b44bd2
Jaid <jaid.jsx@gmail.com>
2019-06-26 00:56:02 +0200

    Removed unneeded `webpack-config-jaid` options
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,8 +4,6 @@ import configure from "webpack-config-jaid"
 
 export default configure({
   publishimo: {fetchGithub: true},
-  optimizeCss: true,
-  createCssFile: true,
   robots: true,
   appDescription: "An alternative Dead by Daylight wiki",
   icon: path.join(__dirname, "icon.png"),
