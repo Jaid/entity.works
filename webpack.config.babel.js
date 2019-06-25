465b7885d10b739a3af22f63b25b3b6a73f8512f
Jaid <jaid.jsx@gmail.com>
2019-06-26 00:57:42 +0200

    manage: Changed app description
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,7 +5,7 @@ import configure from "webpack-config-jaid"
 export default configure({
   publishimo: {fetchGithub: true},
   robots: true,
-  appDescription: "An alternative Dead by Daylight wiki",
+  appDescription: "A moderately/considerably/tremendously modern Dead by Daylight wiki",
   icon: path.join(__dirname, "icon.png"),
   extra: {
     resolve: {
