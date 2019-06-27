332b5f16a73a0b12bd346990dfc3f527f9c35f31
Jaid <jaid.jsx@gmail.com>
2019-06-27 16:55:47 +0200

    Improved internal route handling
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,25 @@
 import React from "react"
 import "react-tippy/dist/tippy.css"
 import DocumentTitle from "react-document-title"
-import {Route, BrowserRouter, Link} from "react-router-dom"
-import IndexPage from "src/pages/index"
-import PatchPage from "src/pages/patch"
+import {Route, BrowserRouter, NavLink} from "react-router-dom"
 import Header from "components/Header"
 import ReactRouterScrollTop from "react-router-scroll-top"
 
+import routes from "./routes"
 import css from "./style.scss"
 
 export default class App extends React.Component {
 
   render() {
+    const routeBlocks = Object.entries(routes).map(([id, path], index) => {
+      return <Route key={index} component={require(`../../pages/${id}`).default} path={path} exact/>
+    })
     return <DocumentTitle title={_PKG_TITLE}>
       <div className={css.container}>
         <BrowserRouter>
           <ReactRouterScrollTop>
             <Header/>
-            <Route component={IndexPage} path="/" exact/>
-            <Route component={PatchPage} path="/patch/:version" exact/>
+            {routeBlocks}
           </ReactRouterScrollTop>
         </BrowserRouter>
       </div>
---
+++ src/components/App/routes.js
@@ -0,0 +1,4 @@
+export default {
+  index: "/",
+  patch: "/patch/:version",
+}
