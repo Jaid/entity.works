46240da30d166cd82e4205bef0215c8cbce22975
Jaid <jaid.jsx@gmail.com>
2019-06-27 19:55:27 +0200

    improve: Added title to `PerkBlock`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/components/PerkBlock/index.js
+++ src/components/PerkBlock/index.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 import classnames from "classnames"
 import Headline from "components/Headline"
 import RichText from "components/RichText"
+import {Link} from "react-router-dom"
 
 import css from "./style.scss"
 
@@ -33,13 +34,18 @@ export default class PerkBlock extends React.Component {
     return <div className={classnames(css.container, this.props.className)}>
       <Headline miniText="Perk" theme="yellow">{this.props.perkInfo.title}</Headline>
       <section className={css.perkInfo}>
-        <img className={css.icon}
-          src={require(`../../data/perks/${this.props.perkInfo.id}/icon.png`)}
-          style={{
-            background: `url(${require(`../../data/perkBackgrounds/${this.props.perkInfo.rarity}.png`)})`,
-            backgroundSize: "contain",
-          }}/>
-        <RichText className={css.effect}>{this.props.perkInfo.effect}</RichText>
+        <Link to={`/perk/${this.props.perkInfo.id}`}>
+          <img className={css.icon}
+            src={require(`../../data/perks/${this.props.perkInfo.id}/icon.png`)}
+            style={{
+              background: `url(${require(`../../data/perkBackgrounds/${this.props.perkInfo.rarity}.png`)})`,
+              backgroundSize: "contain",
+            }}/>
+        </Link>
+        <div>
+          <Link to={`/perk/${this.props.perkInfo.id}`}><div className={css.title}>{this.props.perkInfo.title}</div></Link>
+          <RichText className={css.effect}>{this.props.perkInfo.effect}</RichText>
+        </div>
       </section>
     </div>
   }
