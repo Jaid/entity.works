f2a20c52d98c9c7edd355b23006f399eb444d49a
Jaid <jaid.jsx@gmail.com>
2019-06-28 04:58:34 +0200

    feat: Added clickable links to survivor names
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/components/SurvivorBox/index.js
+++ src/components/SurvivorBox/index.js
@@ -4,6 +4,7 @@ import classnames from "classnames"
 import survivors from "lib/survivors"
 import SurvivorTooltip from "components/SurvivorTooltip"
 import Tooltip from "components/Tooltip"
+import SurvivorLink from "components/SurvivorLink"
 
 import css from "./style.scss"
 
@@ -23,7 +24,7 @@ export default class SurvivorBox extends React.Component {
     const info = survivors.find(({id}) => id === this.props.survivor)
     const content = <span className={classnames(css.container, this.props.className, css[this.props.large ? "large" : "inline"])}>
       <img className={css.icon} src={require(`../../data/survivors/${this.props.survivor}/icon.png`)}/>
-      {info.shortTitle}
+      <SurvivorLink info={info}>{info.shortTitle}</SurvivorLink>
     </span>
     return <Tooltip html={<SurvivorTooltip info={info}/>} noPadding>{content}</Tooltip>
   }
