ff0f57db971516518deec174a3a7b974c3ea4efc
Jaid <jaid.jsx@gmail.com>
2019-12-29 16:54:13 +0100

    Simplified `/perk` pages
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/components/PerkBlock/index.js
+++ src/components/PerkBlock/index.js
@@ -1,10 +1,12 @@
 import classnames from "classnames"
 import PropTypes from "prop-types"
 import React from "react"
+import replaceString from "replace-string"
 
 import findObject from "lib/findObject"
 import Headline from "components/Headline"
 import KillerBox from "components/KillerBox"
+import PerkImage from "components/PerkImage"
 import PerkLink from "components/PerkLink"
 import RichText from "components/RichText"
 import SurvivorBox from "components/SurvivorBox"
@@ -67,23 +69,18 @@ export default class PerkBlock extends React.Component {
         </div>
       }
     }
-    const imgSrc = require(`../../data/perks/${this.props.perkInfo.id}/icon.png`).default
-    const backgroundSrc = require(`../../data/perkBackgrounds/${this.props.perkInfo.rarity}.png`).default
     return <div className={classnames(css.container, this.props.className)}>
       <Headline miniText="Perk" theme="yellow">{this.props.perkInfo.title}</Headline>
       <section className={css.perkInfo}>
-        <img className={css.icon}
-          src={imgSrc}
-          style={{
-            background: `url(${backgroundSrc})`,
-            backgroundSize: "contain",
-          }}/>
+        <PerkImage className={css.icon} perkId={this.props.perkInfo.id}/>
         <div className={css.description}>
           <div className={css.basicInfo}>
             <PerkLink className={css.title} perkInfo={this.props.perkInfo}/>
             {getOwnerNode()}
           </div>
-          <RichText className={css.effect}><RichText className={css.effect}>{this.props.perkInfo.effect.replace(/{this}/g, `{${this.props.perkInfo.id}}`)}</RichText></RichText>
+          <RichText className={css.effect}>
+            {replaceString(this.props.perkInfo.effect, "{this}", /{this}/g, this.props.perkInfo.title)}
+          </RichText>
         </div>
       </section>
     </div>
