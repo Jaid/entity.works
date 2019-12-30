3fbd4ff7fac8bc9c7f0753940e6dab11fcb297ea
Jaid <jaid.jsx@gmail.com>
2019-12-30 22:04:50 +0100

    Added new mobile nav
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/components/NavigationPage/index.js
+++ src/components/NavigationPage/index.js
@@ -45,16 +45,32 @@ export default class NavigationPage extends React.Component {
     links: PropTypes.arrayOf(PropTypes.object).isRequired,
   }
 
-  render() {
+  getDesktopNavigation() {
     const linkNodes = this.props.links.map(({text, to}) => {
       return <NavLink key={text} activeClassName={css.activeLink} className={css.link} to={to}>{text}</NavLink>
     })
-    return <main className={classnames(css.container, this.props.className)}>
-      <div className={css.desktopNavigation}>
-        <Sticky>
-          <nav className={classnames(css.navigation, this.props.navigationClassName)}>{linkNodes}</nav>
-        </Sticky>
+    return <div className={css.desktopNavigation}>
+      <Sticky>
+        <nav className={classnames(css.navigation, this.props.navigationClassName)}>{linkNodes}</nav>
+      </Sticky>
+    </div>
+  }
+
+  getMobileNavigation() {
+    const linkNodes = this.props.links.map(({text, to}) => {
+      return <NavLink key={text} activeClassName={css.activeLink} className={css.link} to={to}>{text}</NavLink>
+    })
+    return <nav className={css.mobileNavigationContainer}>
+      <div className={css.mobileNavigation}>
+        {linkNodes}
       </div>
+    </nav>
+  }
+
+  render() {
+    return <main className={classnames(css.container, this.props.className)}>
+      {this.getDesktopNavigation()}
+      {this.getMobileNavigation()}
       <section className={classnames(css.content, this.props.contentClassName)}>{this.props.children}</section>
     </main>
   }
