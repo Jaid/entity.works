1b234caa63118617107676be28952dcc933adb9a
Jaid <jaid.jsx@gmail.com>
2020-02-14 13:09:03 +0100

    Added update forcing as described here: https://github.com/NekR/offline-plugin/blob/master/docs/updates.md
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,7 +6,27 @@ import App from "components/App"
 
 import store from "src/redux/productionStore"
 
-require("offline-plugin/runtime").install()
+const offlineRuntime = require("offline-plugin/runtime")
+
+offlineRuntime.install({
+  onUpdating: () => {
+    console.log("SW Event:", "onUpdating")
+  },
+  onUpdateReady: () => {
+    console.log("SW Event:", "onUpdateReady")
+    // Tells to new SW to take control immediately
+    offlineRuntime.applyUpdate()
+  },
+  onUpdated: () => {
+    console.log("SW Event:", "onUpdated")
+    // Reload the webpage to load into the new version
+    window.location.reload()
+  },
+
+  onUpdateFailed: () => {
+    console.log("SW Event:", "onUpdateFailed")
+  },
+})
 
 const rootNode = document.createElement("div")
 document.body.append(rootNode)
