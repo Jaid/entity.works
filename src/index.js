3b0ec88d27dfc0ab53413969a96a791e6aabd7a0
Jaid <jaid.jsx@gmail.com>
2020-02-16 14:58:29 +0100

    Formatted
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import offlineRuntime from "offline-plugin/runtime"
 import React from "react"
 import ReactDom from "react-dom"
 import {Provider} from "react-redux"
@@ -5,8 +6,6 @@ import {Provider} from "react-redux"
 import App from "components/App"
 
 import store from "src/redux/productionStore"
-
-const offlineRuntime = require("offline-plugin/runtime")
 
 offlineRuntime.install({
   onUpdating: () => {
